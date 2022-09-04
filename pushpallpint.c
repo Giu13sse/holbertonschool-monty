1872388e437cea56a2063376153396ff072f786b
Giu13sse <giussedipietro@gmail.com>
2022-09-04 11:39:01 -0500

    memory

--- pushpallpint.c
+++ pushpallpint.c
@@ -55,11 +55,10 @@ void pint(stack_t **stack, unsigned int line_number)
 
 	if (tmp == NULL)
 	{
-		fprintf(stderr, "L%d:can't pint, stack empty", line_number);
-		
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit_free(*stack);
+		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", tmp->n);
-	exit_free(*stack);
-	exit(EXIT_FAILURE);
 }
 
